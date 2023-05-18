a74edb2dc093964c08eff69ced749032a18bf8ee
mavinii <pg.marcosoliveira@icloud.com>
2023-05-18 15:06:13 +0100

    Back-end config is ready

---
+++ README.md
@@ -0,0 +1,19 @@
+# TimeLine Memories
+Project to save memories in a timeline
+
+## Back-end:
+- NodeJS
+- Typescript + TSX
+- Fastify (Framework)
+- ESLint
+- Prisma + SQLite
+
+## Front-end:
+- ReactJS + NextJS
+- TailwindCSS
+- ESLint + Prettier Tailwind
+
+## Mobile:
+- Expo
+- NativeWind
+- ESLint + Prettier Tailwind
---
+++ server/src/server.ts
@@ -0,0 +1,21 @@
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const app = fastify()
+const prisma = new PrismaClient()
+
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+  return users
+})
+
+// Fastify routes using promises
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => console.log('Server is running on port http://localhost:3333'))
+  .catch((err) => {
+    console.log(err)
+    process.exit(1)
+  })
