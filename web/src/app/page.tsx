627ba05a8b4a169e677108721c080f951c72002c
mavinii <pg.marcosoliveira@icloud.com>
2023-05-22 21:55:58 +0100

    Mobile environment is ready

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <div className="m-10 rounded-lg text-2xl font-semibold">Hello World</div>
+    <div className="h-screen bg-zinc-900 p-6 text-zinc-50">
+      <h1 className="text-3xl">TimeLine Memories</h1>
+    </div>
   )
 }
